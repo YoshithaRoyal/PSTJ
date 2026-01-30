28bb63ad539ef2e45033eb3ae8852c4e0512363f
YoshithaRoyal <syoshitharoyal@gmail.com>
2026-01-30 11:31:20 +0530

    Refactor array class to binary search implementation

--- Week1/task3.java
+++ Week1/task3.java
@@ -1,15 +1,32 @@
-import java.util.Scanner;
-public class array{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a =new int[n];
-        System.out.println("enter the elements of the array");
-        for (int i = 0;i<n;i++){
-            a[i] = sc.nextInt();
+import java.util.*;
+public class binary{
+    public static int binarysearch(int arr[],int target){
+        int left = 0;
+        int right = arr.length -1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if (arr[mid] == target){
+                return mid;
+            }
+            else if (arr[mid]<target){
+                left = mid +1;
+            }
+            else{
+                right = mid -1;
+            }
         }
-        int j = sc.nextInt();
-        System.out.println("elements at index" +j+"is"+a[j]);
+        return -1;  
         
+    }
+    public static void main(String[] args){
+        int arr[] = {2,3,4,10,40};
+        int target = 2;
+        int result = binarysearch (arr,target);
+        if(result == -1){
+            System.out.println("elemnts not found");
+        }
+        else{
+            System.out.println("element found at index" + result);
+        }
     }
 }
